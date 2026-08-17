d9199bbadc3ba57d139ac2be4c19d1ff282ae258
maganoegi <platserg94@yahoo.com>
2026-08-17 20:58:42 +0200

    hint addded

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,12 +1,12 @@
 
 import requests
-import json
 
 
 def fetch_ohlcv(ticker: str, period: str = "1y") -> list:
     HEADERS = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
     }
+    # hint: formatted strings
     URL = "https://query1.finance.yahoo.com/v8/finance/chart/AAPL?range=1y&interval=1d"
     response = requests.get(URL, params=[], headers=HEADERS)
     response_as_dict = response.json()
