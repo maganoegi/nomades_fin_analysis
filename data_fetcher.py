e4c76d6547e44269a6e0afd93cf969241ff733f0
maganoegi <platserg94@yahoo.com>
2026-08-17 20:41:41 +0200

    added basic structure

---
+++ data_fetcher.py
@@ -0,0 +1,9 @@
+
+
+
+
+def fetch_ohlcv(ticker: str, period: str = "1y") -> list:
+    print("fetching data...")
+
+def save_to_csv(data: list, filepath: str) -> None:
+    print("saving your data to CSV")
---
+++ main.py
@@ -0,0 +1,13 @@
+
+import data_fetcher # module where you got your custom logic
+
+
+
+# HERE YOU WILL DEFINE STUFF
+
+
+if __name__ == '__main__': # separator between definitions and functional code
+    my_data = data_fetcher.fetch_ohlcv("MSFT")
+
+    data_fetcher.save_to_csv(my_data, "./data/my_data.csv")
+    ... # YOUR CODE WILL RUN HERE
