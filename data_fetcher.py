4a825a5047d3453b5902051404d6928dcbfbc446
maganoegi <platserg94@yahoo.com>
2026-08-17 20:53:56 +0200

    added example of requests

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,15 +1,17 @@
 
 import requests
+import json
 
 
 def fetch_ohlcv(ticker: str, period: str = "1y") -> list:
     HEADERS = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
     }
+    URL = "https://query1.finance.yahoo.com/v8/finance/chart/AAPL?range=1y&interval=1d"
+    response = requests.get(URL, params=[], headers=HEADERS)
+    response_as_dict = response.json()
+    print(response_as_dict)
 
-    response = requests.get(url, params=params, headers=HEADERS)
-
-    print("fetching data...")
 
 def save_to_csv(data: list, filepath: str) -> None:
     print("saving your data to CSV")
--- main.py
+++ main.py
@@ -9,5 +9,5 @@ import data_fetcher # module where you got your custom logic
 if __name__ == '__main__': # separator between definitions and functional code
     my_data = data_fetcher.fetch_ohlcv("MSFT")
 
-    data_fetcher.save_to_csv(my_data, "./data/my_data.csv")
+    #data_fetcher.save_to_csv(my_data, "./data/my_data.csv")
     ... # YOUR CODE WILL RUN HERE
